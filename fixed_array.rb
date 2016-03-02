391ffb76c6d38c141166e80befc176e76772e1a3
Eunice Do <eunicedx@gmail.com>
2016-03-02 15:05:09 -0500

    Implement FixedArray required methods

--- fixed_array.rb
+++ fixed_array.rb
@@ -1,3 +1,22 @@
 class FixedArray
+  def new(size)
+    @fixed = Array.new(size)
+  end
 
+  def get(index)
+    raise IndexError if (index < 0 || index > @fixed.length - 1)
+    @fixed[index]
+  end
+
+  def set(index, element)
+    raise IndexError if (index < 0 || index > @fixed.length - 1)
+    @fixed[index] = element
+    @fixed[index]
+  end
 end
+
+# require './fixed_array.rb'
+# abc = FixedArray.new
+# abc.new(3)
+# abc.set(2, "lol")
+# abc.get(2)
